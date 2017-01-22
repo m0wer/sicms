82f050cbede8dbc6cce17977510b83708cbeebf9
m0u <m0u@openmailbox.org>
2017-01-22 11:04:59 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8">
+	<title>Site Title</title>
+		
+</head>
+<body>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+	<div class="square">
+			<img src="img/lentejas.jpg" alt="Lentejas con Arroz" class="img"/>
+			<h2>Lentejas con Arroz</h2>
+	</div>
+</body>
+</html>
